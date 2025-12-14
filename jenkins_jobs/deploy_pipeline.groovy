81e3d24dde710483b24a13781e05ebf82a86fec4
sirartem001 <sirartem001@gmail.com>
2025-12-14 13:40:29 +0300

    fix

--- jenkins_jobs/deploy_pipeline.groovy
+++ jenkins_jobs/deploy_pipeline.groovy
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Git Clone Ansible Repo') {
             steps {
-                git url: 'https://github.com/your-username/ansible-repo.git', branch: 'main'
+                git url: 'https://github.com/sirartem001/ansible_deploy.git', branch: 'main'
             }
         }
         
